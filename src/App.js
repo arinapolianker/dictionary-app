95b7d72e3f20b499cabbf85d7a6c60d3d9f7dba2
arinapolianker <arinapolianker@gmail.com>
2022-01-27 16:06:40 +0200

    Api call + search engine

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
+import React, { useState } from "react";
 import "./App.css";
+import Footer from "./Footer";
+import axios from "axios";
 
 function App() {
+  const [keyword, setKeyword] = useState();
+
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+
+  function search(event) {
+    event.preventDefault();
+    let url = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(url).then(handleResponse);
+  }
+  function word(event) {
+    setKeyword(event.target.value);
+  }
+
   return (
     <div>
       <h1>Dictionary</h1>
+      <form onSubmit={search}>
+        <input type="text" placeholder="search..." onChange={word} />
+        <button>search</button>
+      </form>
+      <Footer />
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+export default function Footer() {
+  return (
+    <div>
+      <span className="footer">
+        Coded by <a href="https://github.com/arinapolianker">Arina Polianker</a>
+        , and an open source on{" "}
+        <a href="https://github.com/arinapolianker/dictionary-app">GitHub</a>
+      </span>
+    </div>
+  );
+}
