1fc5e92475ea6c28edea542deb294233cae85640
arinapolianker <arinapolianker@gmail.com>
2022-01-28 16:49:57 +0200

    Synonyms

--- src/Meaning.js
+++ src/Meaning.js
@@ -6,8 +6,17 @@ export default function Meaning(props) {
         {props.meaning.definitions.map((definition, index) => {
           return (
             <div key={index}>
-              <p>{definition.definition}</p>
+              <p>Definition: {definition.definition}</p>
               <em>Example: {definition.example}</em>
+              <ul>
+                {definition.synonyms.map((synonym, index) => {
+                  if (index < 5) {
+                    return <li key={index}>{synonym}</li>;
+                  } else {
+                    return null;
+                  }
+                })}
+              </ul>
             </div>
           );
         })}
