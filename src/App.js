e7cbff9cfb5b5df77bc54d652f330e599da1ac46
arinapolianker <arinapolianker@gmail.com>
2022-01-29 14:50:22 +0200

    Photos problem fix

--- src/App.js
+++ src/App.js
@@ -10,6 +10,17 @@ function App() {
   const [word, setWord] = useState();
   const [photos, setPhotos] = useState();
 
+  function search(event) {
+    event.preventDefault();
+    let url = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(url).then(handleDictionaryResponse);
+    let pexelsApiakey =
+      "563492ad6f91700001000001f418c91d75d64ee88808e11dce0c8a29";
+    let pexelsUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+    axios
+      .get(pexelsUrl, { headers: { Authorization: `Bearer ${pexelsApiakey}` } })
+      .then(handlePexlesResponse);
+  }
   function handleDictionaryResponse(response) {
     console.log(response.data[0]);
     setWord(response.data[0]);
@@ -20,18 +31,6 @@ function App() {
   }
   function enterWord(event) {
     setKeyword(event.target.value);
-  }
-  function search(event) {
-    event.preventDefault();
-    let url = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-    axios.get(url).then(handleDictionaryResponse);
-
-    let pexelsApiakey =
-      "563492ad6f91700001000001f418c91d75d64ee88808e11dce0c8a29";
-    let pexelsUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
-    axios
-      .get(pexelsUrl, { headers: { Authorization: `Bearer ${pexelsApiakey}` } })
-      .then(handlePexlesResponse);
   }
 
   return (
--- src/Photos.js
+++ src/Photos.js
@@ -1,16 +1,22 @@
 export default function Photos(props) {
-  return (
-    <div className="container">
-      {props.photos.map((photo, index) => {
-        return (
-          <img
-            src={photo.src.landscape}
-            alt={photo.alt}
-            key={index}
-            className="pic"
-          />
-        );
-      })}
-    </div>
-  );
+  if (props.photos) {
+    return (
+      <div className="container">
+        {props.photos.map((photo, index) => {
+          return (
+            <a href={photo.src.original} target="_blank" rel="noreferrer">
+              <img
+                src={photo.src.landscape}
+                alt={photo.alt}
+                key={index}
+                className="pic"
+              />
+            </a>
+          );
+        })}
+      </div>
+    );
+  } else {
+    return null;
+  }
 }
